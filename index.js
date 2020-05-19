f5a22e0ed77f878b3f1c25807f5d803feaf136d2
VAIBHAV SHUKLA <shuklavaibhav0306@gmail.com>
2020-05-19 14:34:32 +0530

    updated

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app=express();
 
 app.set('view engine', 'ejs');
 
-mongoose.connect('mongodb+srv://terminator:testdb@rest-api-ag54g.mongodb.net/test?retryWrites=true&w=majority',{ useNewUrlParser: true , useUnifiedTopology: true });
+mongoose.connect('mongodb+srv://terminator:testdb@rest-api-ag54g.mongodb.net/test',{ useNewUrlParser: true , useUnifiedTopology: true });
 
 app.use(express.static('public'));
 
